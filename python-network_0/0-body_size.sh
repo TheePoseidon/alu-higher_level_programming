c50fdcfb34e1a1da6f34394b30993dd130c2bfa3
TheePoseidon <169036596+TheePoseidon@users.noreply.github.com>
2024-07-28 21:10:18 +0200

    Task 0

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends request to given URL and displays the size of the body of response
+# curl a URL and display the size of the response in bytes
 curl -so /dev/null -w '%{size_download}\n' "$1"
