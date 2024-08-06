4bad489751860dc04178d996ffddaff7a3f4b5e9
El-gibbor <chiagoziem11@yahoo.com>
2024-08-06 08:41:57 +0200

    modeule documented

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" fetches https://alx-intranet.hbtn.io/status """
+""" fetches resource from https://alx-intranet.hbtn.io/status"""
+import urllib.request as request
 
-from urllib import request
-
-with request.urlopen("https://alx-intranet.hbtn.io/status") as fetched:
-    response = fetched.read()
-
-    print("Body response:")
-    print(f"\t- type: {type(response)}")
-    print(f"\t- content: {response}")
-    print(f"\t- utf8 content: {response.decode('utf8')}")
+if __name__ == "__main__":
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
