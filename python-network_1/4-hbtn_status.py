181bf759acf9710390783afe4826d0b3557f58da
El-gibbor <chiagoziem11@yahoo.com>
2024-08-06 09:34:56 +0200

    modified to print custom status code

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
+"""
+Python script that fetches https://alu-intranet.hbtn.io/status
+"""
 import requests
 
-
-if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.status_code))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
