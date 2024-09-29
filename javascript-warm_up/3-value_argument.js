d9cc64bef63fe6d4ece8a247958406c5cdc10156
TheePoseidon <169036596+TheePoseidon@users.noreply.github.com>
2024-09-29 21:26:09 +0300

    Prints the first argument

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
-if (myArgs[0]) {
-    console.log(myArgs[0]);
-} else {
-    console.log('No argument');
-}
+const { argv } = require('process');
+
+const msg = (argv[2] === undefined) ? 'No argument' : argv[2];
+console.log(msg);
